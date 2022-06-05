2931ca8773c48d12a8ba80a8cdeee61131cc5256
hiroshisasmita <hiroshisasmita@gmail.com>
2022-06-05 16:54:09 +0700

    refactor

--- src/main/java/com/hiroshisasmita/android_core/base/BaseRetrofitResponseAdapter.kt
+++ src/main/java/com/hiroshisasmita/android_core/base/BaseRetrofitResponseAdapter.kt
@@ -6,12 +6,12 @@ import retrofit2.Call
 import retrofit2.Response
 import java.lang.reflect.Type
 
-abstract class BaseRetrofitResponseAdapter<C: Any, V: Any> {
+abstract class BaseRetrofitResponseAdapter<Before: Any, Result: Any> {
 
     /**
      * Function to handle asynchronous api
      */
-    suspend fun handleApi(block: suspend () -> Response<C>): ResultState<V> {
+    suspend fun handleApi(block: suspend () -> Response<Before>): ResultState<Result> {
         return try {
             val response = block.invoke()
             proceed(response)
@@ -24,7 +24,7 @@ abstract class BaseRetrofitResponseAdapter<C: Any, V: Any> {
     /**
      * Function to handle synchronous api
      */
-    fun handleApiSynchronously(block: () -> Call<C>): ResultState<V> {
+    fun handleApiSynchronously(block: () -> Call<Before>): ResultState<Result> {
         return try {
             val response = block.invoke().execute()
             proceed(response)
@@ -38,7 +38,7 @@ abstract class BaseRetrofitResponseAdapter<C: Any, V: Any> {
      * Function to proceed api call
      */
     @Throws(Exception::class)
-    private fun proceed(response: Response<C>): ResultState<V> {
+    private fun proceed(response: Response<Before>): ResultState<Result> {
         if (!response.isSuccessful) {
             throw response.errorBody()?.let {
                 fetchError(it, response.code())
@@ -54,7 +54,7 @@ abstract class BaseRetrofitResponseAdapter<C: Any, V: Any> {
      * Function to handle data when response isSuccessful
      */
     @Throws(Exception::class)
-    protected abstract fun fetchSuccessData(data: C): ResultState<V>
+    protected abstract fun fetchSuccessData(data: Before): ResultState<Result>
 
     /**
      * Function to handle data when response isError
--- src/main/java/com/hiroshisasmita/android_core/base/BaseRetrofitResponsePlainAdapter.kt
+++ src/main/java/com/hiroshisasmita/android_core/base/BaseRetrofitResponsePlainAdapter.kt
@@ -6,14 +6,13 @@ import retrofit2.Call
 import retrofit2.Response
 import java.lang.reflect.Type
 
-abstract class BaseRetrofitResponsePlainAdapter<C: Any, V: Any> {
+abstract class BaseRetrofitResponsePlainAdapter<Before: Any, Result: Any> {
 
     /**
      * Function to handle asynchronous api
      */
-    suspend fun handleApi(block: suspend () -> Response<C>): V? {
+    suspend fun handleApi(response: Response<Before>): Result? {
         return try {
-            val response = block.invoke()
             proceed(response)
         }catch (e: Exception) {
             throw e
@@ -24,7 +23,7 @@ abstract class BaseRetrofitResponsePlainAdapter<C: Any, V: Any> {
     /**
      * Function to handle synchronous api
      */
-    fun handleApiSynchronously(block: () -> Call<C>): V? {
+    fun handleApiSynchronously(block: () -> Call<Before>): Result? {
         return try {
             val response = block.invoke().execute()
             proceed(response)
@@ -38,7 +37,7 @@ abstract class BaseRetrofitResponsePlainAdapter<C: Any, V: Any> {
      * Function to proceed api call
      */
     @Throws(Exception::class)
-    private fun proceed(response: Response<C>): V? {
+    private fun proceed(response: Response<Before>): Result? {
         if (!response.isSuccessful) {
             throw response.errorBody()?.let {
                 fetchError(it, response.code())
@@ -54,7 +53,7 @@ abstract class BaseRetrofitResponsePlainAdapter<C: Any, V: Any> {
      * Function to handle data when response isSuccessful
      */
     @Throws(Exception::class)
-    protected abstract fun fetchSuccessData(data: C): V?
+    protected abstract fun fetchSuccessData(data: Before): Result?
 
     /**
      * Function to handle data when response isError
